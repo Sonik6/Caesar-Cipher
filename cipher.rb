3060cb592eb70f2fb8cddc0c5bafc7189d24fe00
Slawek Galka <galka.slawek6@gmail.com>
2021-12-03 21:46:29 +0100

    finished project

---
+++ cipher.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+def caesar_cipher(to_code, shift)
+  ascii = to_code.chars.map(&:ord)
+
+  transformed = ascii.map { |char| char + shift.to_i }
+
+  transformed.map(&:chr).join
+end
